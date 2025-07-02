9def949c8780b4faa8aa16032a979a5f386d7ee0
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-07-02 22:03:16 +0100

    Update travelgear.js

--- travelgear.js
+++ travelgear.js
@@ -1,4 +1,12 @@
 function getGear()
 {
   console.log("Get Gear...");
+  let itemResults = [];
+  fetch('disneycases.json')
+  .then(response => response.json())
+  .then(data => {
+    itemResults = data;
+    console.log("Disney Suitcase Loaded:", itemResults);
+  })
+  .catch(error => console.error("Error loading item data:", error));
 }
