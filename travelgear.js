c31afd75e24b7fd9bc94338727375a2a6986e8ff
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-07-12 13:07:48 +0100

    Update travelgear.js

--- travelgear.js
+++ travelgear.js
@@ -50,9 +50,9 @@ function getGear()
        if (asinLookup == item.ASIN)
        {
          console.log("***Match Found***");
-         const additionalInfoHeader = document.createElement("h2");
-         additionalInforHeader.textContent = "Additional Information";
-         gearCard.appendChild(additionalInforHeader);
+         const infoHeader = document.createElement("h2");
+         infoHeader.textContent = "Additional Information";
+         gearCard.appendChild(infoHeader);
          const notes = document.createElement("p");
          notes = entry.notes;
          gearCard.appendChild(notes);
